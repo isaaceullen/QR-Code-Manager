e3e5880710f660d9519f0b5485bd0a90d73ef249
Isaac Eullen <146856994+isaaceullen@users.noreply.github.com>
2026-03-19 13:26:37 -0300

    feat: Update app title and language
    Changes the application title from "QR Code Camerite" to "Contato Camerite" and sets the default language to Portuguese (pt-br).

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,13 +2,13 @@ import type {Metadata} from 'next';
 import './globals.css'; // Global styles
 
 export const metadata: Metadata = {
-  title: 'QR Code Camerite',
-  description: 'QR Code Camerite',
+  title: 'Contato Camerite',
+  description: 'Contato Camerite',
 };
 
 export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
-    <html lang="en">
+    <html lang="pt-br">
       <body suppressHydrationWarning>{children}</body>
     </html>
   );
